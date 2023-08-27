c1f2e70d705ba36d6fbd366ec4b6a45979b5a432
Will7136 <89648548+Will7136@users.noreply.github.com>
2023-08-27 22:53:33 +0100

    Add files via upload

---
+++ Week9/Department.java
@@ -0,0 +1,24 @@
+package project;
+import java.util.Map;
+
+public class Department {
+    private String name;
+    private Map<String, worker> workers;
+    private Map<String, Project> projects;
+
+    public Department(String depName){
+        name = depName;
+    }
+
+    public worker getWorker(String newID){
+            return(workers.get(newID));
+    }
+
+    public Project getProject(String newID){
+        return(projects.get(newID));
+    }
+
+    public Project createProject(String newID, String newTitle){
+
+    }
+}
